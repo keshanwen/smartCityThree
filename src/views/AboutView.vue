1d567e0447d386961bb4b7513e42fb488a093fef
keshanwen <3640@winnermedical.com>
2024-05-13 09:33:40 +0800

    feat: 初始化 demo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div ref="container" class="about">
   </div>
 </template>
 
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import * as THREE from "three";
+import gsap from "gsap";
+// 导入gui对象
+import gui from "@/threes/gui.js";
+// 导入场景
+import scene from "@/threes/scene";
+// 导入相机
+import camera from "@/threes/camera";
+// 导入控制器
+import controls from "@/threes/controls";
+// 导入辅助坐标轴
+import axesHelper from "@/threes/axesHelper";
+// 导入渲染器
+import renderer from "@/threes/renderer";
+// 初始化调整屏幕
+import "@/threes/init";
+// 导入每一帧的执行函数
+import animate from "@/threes/animate";
+
+
+let container = ref()
+scene.add(camera)
+scene.add(axesHelper)
+
+
+onMounted(() => {
+  container.value.appendChild(renderer.domElement)
+  animate();
+})
+
+
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
