465102ebf59417286c19091aa9a9b0f561fb13dc
keshanwen <3640@winnermedical.com>
2024-05-23 13:54:04 +0800

    feat: 将 changgview 抽离出来

--- src/views/editor/components/rightAttribute/scene/scene.vue
+++ src/views/editor/components/rightAttribute/scene/scene.vue
@@ -50,7 +50,7 @@
         </div>
       </div>
       <div class="data-wrap">
-         <div v-for="item in threeStore.config.scene.view" :key="item.name" @click="() => changeView(item)" class="view-item">
+         <div v-for="item in threeStore.config.scene.view" :key="item.name" @click="() => changeViewClick(item)" class="view-item">
           {{ item.name }}
          </div>
       </div>
@@ -62,8 +62,7 @@ import { ref, reactive, inject, onMounted } from 'vue';
 import * as THREE from 'three';
 import { useThreeStore } from '@/stores/editor';
 import InitThree from '@/views/editor/three/index';
-import gsap from "gsap";
-import TWEEN from '@tweenjs/tween.js';
+import { changeView } from '@/views/editor/three/changeView'
 
 const app = inject('app') as InitThree;
 const threeStore = useThreeStore();
@@ -118,47 +117,18 @@ function confirmViewSet() {
   resetState()
 }
 
-const changeView = (item: any) => {
-    const tween = new TWEEN.Tween({
-            // 相机开始坐标
-            x: app.camera.position.x,
-            y: app.camera.position.y,
-            z: app.camera.position.z,
-            // 相机开始指向的目标观察点
-            tx: app.controls.target.x,
-            ty: app.controls.target.y,
-            tz: app.controls.target.z,
-        })
-        .to({
-            // 相机结束坐标
-            x:  item.position.x,
-            y:  item.position.y,
-            z:  item.position.z,
-            // 相机结束指向的目标观察点
-            tx: item.target.x,
-            ty: item.target.y,
-            tz: item.target.z,
-        }, 3000)
-        .onUpdate(function (obj) {
-          // 动态改变相机位置
-          app.camera.position.set(obj.x, obj.y, obj.z);
-          // 动态计算相机视线
-          app.controls.target.set(obj.tx, obj.ty, obj.tz)
-          app.controls.update() // 内部执行 lookAT()
-        })
-     .start()
-     .onComplete(() => {
-        // 完成的回调
-        console.log('完成~~~~~~~~~')
-     })
-    .easing(TWEEN.Easing.Sinusoidal.InOut); //使用二次缓动函数
-
-    function loop() {
-        tween.update();
-        window.requestAnimationFrame(loop);
-      }
-
-     loop()
+const changeViewClick = (item: any) => {
+  const params = {
+    // 相机结束坐标
+    x:  item.position.x,
+    y:  item.position.y,
+    z:  item.position.z,
+    // 相机结束指向的目标观察点
+    tx: item.target.x,
+    ty: item.target.y,
+    tz: item.target.z,
+  }
+  changeView(app, params)
 }
 
 
---
+++ src/views/editor/three/changeView.ts
@@ -0,0 +1,50 @@
+import TWEEN from '@tweenjs/tween.js';
+import InitThree from '@/views/editor/three/index';
+
+
+export function changeView(app: InitThree, target: any = {}) {
+  const { x = 0,y = 0,z = 0, tx = 0, ty = 0, tz = 0} = target
+  const tween = new TWEEN.Tween({
+    // 相机开始坐标
+    x: app.camera.position.x,
+    y: app.camera.position.y,
+    z: app.camera.position.z,
+    // 相机开始指向的目标观察点
+    tx: app.controls.target.x,
+    ty: app.controls.target.y,
+    tz: app.controls.target.z,
+  })
+    .to(
+      {
+        // 相机结束坐标
+        x,
+        y,
+        z,
+        // 相机结束指向的目标观察点
+        tx,
+        ty,
+        tz,
+      },
+      3000
+    )
+    .onUpdate(function (obj) {
+      // 动态改变相机位置
+      app.camera.position.set(obj.x, obj.y, obj.z);
+      // 动态计算相机视线
+      app.controls.target.set(obj.tx, obj.ty, obj.tz);
+      app.controls.update(); // 内部执行 lookAT()
+    })
+    .start()
+    .onComplete(() => {
+      // 完成的回调
+      console.log('完成~~~~~~~~~');
+    })
+    .easing(TWEEN.Easing.Sinusoidal.InOut); //使用二次缓动函数
+
+  function loop() {
+    tween.update();
+    window.requestAnimationFrame(loop);
+  }
+
+  loop();
+}
